aa0b6a77918cee4b0e53ed0422960b1c44123bc7
Afedra Vincent <afedravnct@gmail.com>
2026-07-29 14:08:42 +0300

    fix: replace remaining FileUp tag with Upload in App.jsx JSX

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import HrEstablishmentsLedger from './HrEstablishmentsLedger';
 import Admin_Communication from './Admin_Communication';
 import BulkNominalRollUpload from './BulkNominalRollUpload';
 
+
 const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 const REGIONAL_HIERARCHY = {
